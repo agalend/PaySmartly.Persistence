4ac36d2db96fb580909ad5b590fa4ac7f5022559
stefan <stefan.bozov@outlook.com>
2023-12-03 17:07:41 +0200

    add OpenTelemetry

--- PaySmartly.Persistance/Program.cs
+++ PaySmartly.Persistance/Program.cs
@@ -1,14 +1,50 @@
 using PaySmartly.Persistance.Mongo;
 using PaySmartly.Persistance.Repository;
 using PaySmartly.Persistance.Services;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+// TODO: set service name somewhere!!!
+string ServiceName = "Legislation Service";
 
 var builder = WebApplication.CreateSlimBuilder(args);
+AddOpenTelemetryLogging(builder);
 
 builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
 builder.Services.AddSingleton<IRepository<MongoRecord>, MongoRepository>();
 builder.Services.AddGrpc();
+AddOpenTelemetryService(builder);
 
 var app = builder.Build();
 app.MapGrpcService<PersistanceService>();
 
 app.Run();
+
+void AddOpenTelemetryLogging(WebApplicationBuilder builder)
+{
+    builder.Logging.AddOpenTelemetry(options =>
+    {
+        ResourceBuilder resourceBuilder = ResourceBuilder.CreateDefault().AddService(ServiceName);
+
+        options.SetResourceBuilder(resourceBuilder).AddConsoleExporter();
+    });
+}
+
+void AddOpenTelemetryService(WebApplicationBuilder builder)
+{
+    OpenTelemetryBuilder openTelemetryBuilder = builder.Services.AddOpenTelemetry();
+
+    openTelemetryBuilder = openTelemetryBuilder.ConfigureResource(resource => resource.AddService(ServiceName));
+
+    openTelemetryBuilder = openTelemetryBuilder.WithTracing(tracing =>
+    {
+        tracing.AddAspNetCoreInstrumentation().AddConsoleExporter();
+    });
+    openTelemetryBuilder = openTelemetryBuilder.WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation().AddConsoleExporter();
+    });
+}
